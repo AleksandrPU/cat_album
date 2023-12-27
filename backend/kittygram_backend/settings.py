768750959fdcbacfce519e1d0ae7d4a84cf81bd5
Паутов Александр <sh2d000w@gmail.com>
2023-12-27 22:26:17 +0300

    fix: correct for review #1

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -4,11 +4,15 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django'),
 
-DEBUG = True
+if os.getenv('DJANGO_DEBUG', 'django').lower() == 'true':
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'django').split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
